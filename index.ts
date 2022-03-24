c1176f4c25a93e9d0cb4ed9645e5338547826ec7
Anton <62949848+icepaq@users.noreply.github.com>
2022-03-24 16:38:03 -0400

    use promise

--- index.ts
+++ index.ts
@@ -1,33 +1,19 @@
 import express from "express"
-const { exec } = require('child_process');
+const exec = require('child-process-promise').exec;
 
 const app = express()
 const port = 3001
 
-app.get('/asteriskcheck', (req: express.Request, res: express.Response) => {
+app.get('/asteriskcheck', async (req: express.Request, res: express.Response) => {
     const command = 'asterisk -x "core show channels"';
-    const output = exec(command, (err: any, stdout: any, stderr: any) => {
-        if (err) {
-            return "Error";
-        }
-        
-        return stdout;
-    });
-
-    console.log(output);
-
-    // const output = "Channel              Location             State   Application(Data)\n" +
-    // "0 active channels\n" +
-    // "0 active calls\n" +
-    // "22 calls processed\n" +
-    // "Asterisk ending (0).";
-
-    if (output === "Error") {
+    const output = await exec(command);
+    
+    if (output.stderr) {
         res.json({status: 400, message: "Asterisk Service is Down"});
         return;
     }
 
-    const split = output.split("\n");
+    const split = output.stdout.split("\n");
     const info = {
         calls: split[2].split(" ")[0],
         processed: split[3].split(" ")[0],
@@ -35,7 +21,15 @@ app.get('/asteriskcheck', (req: express.Request, res: express.Response) => {
 
     const message = "Asterisk Service is Running | " + info.calls + " active calls | " + info.processed + " calls processed";
 
+    console.log(message);
     res.json({status: 200, message: message});
+
+
+    // const output = "Channel              Location             State   Application(Data)\n" +
+    // "0 active channels\n" +
+    // "0 active calls\n" +
+    // "22 calls processed\n" +
+    // "Asterisk ending (0).";
 })
 
 app.listen(port, () => {
